2d6549b06855719042ab41188d170ef16ce35897
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-03-16 20:12:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++)
         for (int j = i + 1; j < len; j++)
-            if (arr[i] + arr[j] == value) 
+            if (arr[i] + arr[j] == value)
                 count++;
     return count;
 }
@@ -11,12 +11,11 @@ int countPairs2(int *arr, int len, int value) {
     int count = 0, k = 0;
     for (int i = 1; i < len; i++)
         if (arr[0] + arr[i] <= value) k++;
-        else {
+        else
             break;
-        }
     for (int i = 0; i < k; i++)
         for (int j = i + 1; j <= k; j++)
-            if (arr[i] + arr[j] == value) 
+            if (arr[i] + arr[j] == value)
                 count++;
     return count;
 }
@@ -28,8 +27,7 @@ int countPairs3(int *arr, int len, int value) {
         if (arr[begin] + arr[m] > value) {
             end = m - 1;
             break;
-        }
-        else {
+        } else {
             i = begin+1;
             while (arr[begin] + arr[i] <= value) {
                 if (arr[begin] + arr[i] == value) count++;
